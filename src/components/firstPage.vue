77ee8cb72a6203ecc094c33bdf4dd99d924d47c3
YuChenLiao <yuchenliao3@gmail.com>
2022-06-27 15:29:03 +0800

    init

---
+++ src/components/firstPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  name: 'firstPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
