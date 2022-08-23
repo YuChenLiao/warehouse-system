3efa3f76f1637ccbd99374fe250d477f7c21d852
YuChenLiao <yuchenliao3@gmail.com>
2022-08-23 11:02:14 +0800

    修改首页样式

--- src/pages/system.vue
+++ src/pages/system.vue
@@ -1,7 +1,12 @@
 <template>
   <el-card>
     <div class="title">系统管理</div>
-    <el-button type="primary">数据库备份</el-button>
+    <el-form class="forms">
+      <el-form-item style="text-align: center">
+        <el-button type="primary">数据库备份</el-button>
+        <el-button type="success">添加新用户</el-button>
+      </el-form-item>
+    </el-form>
     <div class="title">用户管理</div>
     <el-table></el-table>
   </el-card>
@@ -9,15 +14,13 @@
 
 <script>
 export default {
-  name: 'system',
-  data () {
-    return {
-    }
-  }
-}
+  name: "system",
+  data() {
+    return {};
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
