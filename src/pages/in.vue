92069485e95c717609bccd4198389a2a727a69ac
YuChenLiao <yuchenliao3@gmail.com>
2022-08-23 08:44:20 +0800

    完成入库登记基本样式

--- src/pages/in.vue
+++ src/pages/in.vue
@@ -1,15 +1,20 @@
 <template>
   <el-card>
     <div class="title">入库登记</div>
-    <el-form class="forms">
+    <el-form class="forms" :model="inForm">
       <el-form-item label="原料名称" label-width="80px">
-        <el-input></el-input>
+        <el-input v-model="inForm.name"></el-input>
       </el-form-item>
       <el-form-item label="入库数量" label-width="80px">
-        <el-input></el-input>
+        <el-input type="number" v-model="inForm.num"></el-input>
       </el-form-item>
-      <el-form-item></el-form-item>
+      <el-form-item label-width="80px">
+        <el-button type="primary">提交</el-button>
+        <el-button type="danger">清空</el-button>
+      </el-form-item>
     </el-form>
+    <div class="title">入库单详情</div>
+    <el-table></el-table>
   </el-card>
 </template>
 
@@ -18,6 +23,10 @@ export default {
   name: 'inHouse',
   data () {
     return {
+      inForm: {
+        name: '',
+        num: 0,
+      }
     }
   }
 }
